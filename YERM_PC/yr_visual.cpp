8c94b12d4948e761d61a003ad29b52b0a03d511f
onart <onart97@naver.com>
2024-10-28 21:00:46 +0900

    visual draw 선후 구조

--- YERM_PC/yr_visual.cpp
+++ YERM_PC/yr_visual.cpp
@@ -1,15 +1,62 @@
 #include "yr_visual.h"
 #include "yr_basic.hpp"
+#include "yr_sys.h"
 
 namespace onart {
+
+	Camera::Camera() {
+		setLookAt({ 0,0,1 }, { 0,0,0 }, { 0,1,0 });
+		setOrthogonal(0, 1);
+	}
+
+	void Camera::setLookAt(const vec3& eye, const vec3& at, const vec3& up) {
+		view.eye = eye;
+		view.at = at;
+		view.up = up;
+		view.matrix = mat4::lookAt(eye, at, up);
+	}
+
+	void Camera::setPerspective(float fovy, float near, float far) {
+		proj.near = near;
+		proj.far = far;
+		proj.fovy = fovy;
+		proj.matrix = mat4::perspective(fovy, proj.viewRange.y / proj.viewRange.x, near, far);
+	}
+
+	void Camera::setOrthogonal(float near, float far) {
+		proj.fovy = 0;
+		proj.near = near;
+		proj.far = far;
+		vec2 iv = vec2(1) / proj.viewRange;
+		proj.matrix = mat4::TRS(vec3(0, 0, near), Quaternion(), vec3(iv.x, iv.y, 1 / (far - near)));
+	}
 
 	VisualElement* Scene::createVisualElement() {
 		return ve.emplace_back(std::make_shared<shp_t<VisualElement>>()).get();
 	}
 
+	IntermediateScene::IntermediateScene(const RenderPassCreationOptions& opts) {		
+		target0 = YRGraphics::createRenderPass(INT32_MIN, opts);
+	}
+
+	IntermediateScene::~IntermediateScene() {
+		for (auto it = pred.begin(); it != pred.end(); ++it) { (*it)->succ.erase(this); }
+		for (auto it = succ.begin(); it != succ.end(); ++it) { (*it)->removePred(this); }
+		for (auto it = succ2.begin(); it != succ2.end(); ++it) { (*it)->removePred(this); }
+	}
+
+	void IntermediateScene::resize(uint32_t width, uint32_t height) {
+		target0->resize(width, height);
+	}
+
 	void IntermediateScene::addPred(IntermediateScene* sc) {
 		sc->succ.insert(this);
 		pred.insert(sc);
+	}
+
+	void IntermediateScene::removePred(IntermediateScene* sc) {
+		sc->succ.erase(this);
+		pred.erase(sc);
 	}
 
 	void IntermediateScene::draw() {
@@ -25,7 +72,18 @@ namespace onart {
 				continue;
 			}
 			if (state.pipeline != elem->pipeline.get()) { state.pipeline = elem->pipeline.get(); target0->usePipeline(state.pipeline, 0); }
-			if (elem->mesh1) { target0->invoke(elem->mesh0); }
+
+			// todo: avoid re-setting the same ones
+			// postponed since vk <-> d3d11, opengl shader resource scheme are different
+			if (elem->textureSet) { target0->bind(elem->textureBind, elem->textureSet); }
+			else if (elem->texture) { target0->bind(elem->textureBind, elem->texture); }
+
+			if (elem->pushed.size()) { target0->push(elem->pushed.data(), 0, elem->pushed.size()); }
+			/* // todo: per-object dynamic uniform buffer scheme & per-frame(scene) uniform buffer scheme
+			if (elem->poub.size()) {
+
+			}
+			*/
 			if (elem->instaceCount > 1) { target0->invoke(elem->mesh0, elem->mesh1, elem->instaceCount, 0, elem->meshRangeCount, elem->meshRangeCount); }
 			else { target0->invoke(elem->mesh0, elem->meshRangeStart, elem->meshRangeCount); }
 		}
@@ -34,5 +92,52 @@ namespace onart {
 		for (auto& pr : pred) { prerequisites[idx++] = pr->target0.get(); }
 
 		target0->execute(succ.size(), pred.size(), prerequisites);
+	}
+
+	FinalScene::FinalScene(const YRGraphics::pRenderPass2Screen& rp) : target0(rp) {}
+
+	void FinalScene::addPred(IntermediateScene* sc) { 
+		pred.insert(sc);
+		sc->succ2.insert(this);
+	}
+
+	void FinalScene::removePred(IntermediateScene* sc) { 
+		pred.erase(sc);
+		sc->succ2.erase(this);
+	}
+	
+	void FinalScene::draw() {
+		if (sorter) { sorter(ve); }
+		struct {
+			YRGraphics::Pipeline* pipeline = nullptr;
+		} state;
+		target0->start();
+		for (auto& elem : ve) {
+			if (elem->fr) {
+				elem->fr->draw(target0.get());
+				std::memset(&state, 0, sizeof(state));
+				continue;
+			}
+			if (state.pipeline != elem->pipeline.get()) { state.pipeline = elem->pipeline.get(); target0->usePipeline(state.pipeline, 0); }
+
+			// todo: avoid re-setting the same ones
+			// postponed since vk <-> d3d11, opengl shader resource scheme are different
+			if (elem->textureSet) { target0->bind(elem->textureBind, elem->textureSet); }
+			else if (elem->texture) { target0->bind(elem->textureBind, elem->texture); }
+
+			if (elem->pushed.size()) { target0->push(elem->pushed.data(), 0, elem->pushed.size()); }
+			/* // todo: per-object dynamic uniform buffer scheme & per-frame(scene) uniform buffer scheme
+			if (elem->poub.size()) {
+
+			}
+			*/
+			if (elem->instaceCount > 1) { target0->invoke(elem->mesh0, elem->mesh1, elem->instaceCount, 0, elem->meshRangeCount, elem->meshRangeCount); }
+			else { target0->invoke(elem->mesh0, elem->meshRangeStart, elem->meshRangeCount); }
+		}
+		YRGraphics::RenderPass* prerequisites[16]{};
+		size_t idx = 0;
+		for (auto& pr : pred) { prerequisites[idx++] = pr->target0.get(); }
+
+		target0->execute(0, pred.size(), prerequisites);
 	}
 }
--- YERM_PC/yr_visual.h
+++ YERM_PC/yr_visual.h
@@ -12,8 +12,8 @@ namespace onart
         friend class Scene;
     public:
         void setViewRange(uint32_t width, uint32_t height);
-        void setOrthogonal();
-        void setPerspective(float fovy);
+        void setOrthogonal(float near, float far);
+        void setPerspective(float fovy, float near, float far);
         void setLookAt(const vec3& eye, const vec3& at, const vec3& up);
     private:
         Camera();
@@ -26,6 +26,8 @@ namespace onart
         struct {
             vec2 viewRange{128, 128};
             float fovy;
+            float near;
+            float far;
             mat4 matrix;
         } proj;
     };
@@ -42,13 +44,17 @@ namespace onart
             YRGraphics::pMesh mesh0; // basic mesh
             YRGraphics::pMesh mesh1; // instance data
             YRGraphics::pTexture texture; // todo: plus lighting parameter
+            YRGraphics::pTextureSet textureSet;
             YRGraphics::pPipeline pipeline;
+            YRGraphics::pUniformBuffer ub;
             std::vector<uint8_t> pushed; // per object push data
-            std::vector<uint8_t> poub; // per object ub data
+            std::vector<uint8_t> poub; // per object ub data -> todo: remove this
             std::unique_ptr<FreeRenderer> fr = nullptr;
             unsigned instaceCount = 1;
             unsigned meshRangeStart = 0;
             unsigned meshRangeCount = 0;
+            unsigned ubBind = 1;
+            unsigned textureBind = 2;
         private:
             int ubIndex; // dynamic ub index
         protected:
@@ -65,26 +71,30 @@ namespace onart
     };
 
     class IntermediateScene: public Scene{
+    friend class FinalScene;
     public:
-        IntermediateScene(uint32_t width, uint32_t height, int targetType);
+        IntermediateScene(const RenderPassCreationOptions&);
         void resize(uint32_t width, uint32_t height);
         void addPred(IntermediateScene* scene);
         void removePred(IntermediateScene* scene);
     private:
         void draw();
+        ~IntermediateScene();
         std::set<IntermediateScene*> pred;
         std::set<IntermediateScene*> succ;
+        std::set<FinalScene*> succ2;
         YRGraphics::pRenderPass target0;
     };
 
     class FinalScene: public Scene{
     public:
-        FinalScene(uint32_t width, uint32_t height);
+        FinalScene(const YRGraphics::pRenderPass2Screen&);
         void addPred(IntermediateScene* scene);
         void removePred(IntermediateScene* scene);
     private:
         void draw();
-        YRGraphics::pRenderPass2Screen target1;
+        std::set<IntermediateScene*> pred;
+        YRGraphics::pRenderPass2Screen target0;
     };
 }
 
